5cfbd1ee3f260c09849ebf0425a417b99ec0d510
Mark Yapp <markwyapp@gmail.com>
2018-12-07 03:29:42 -0700

    add seed data

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 $(".login-button").click(function (event) {
   event.preventDefault();
   $("#log-in").addClass("hidden");
-  // $("#sign-up").addClass("hidden");
+  $("#sign-up").addClass("hidden");
   $(".user-page").removeClass("hidden");
 })
 
@@ -86,3 +86,52 @@ $(function listenForResults() {
 //     `${now.toLocaleDateString()} ${now.toLocaleTimeString()} ${req.method} ${req.url}`);
 //   next();
 // }
+
+
+var mock_entries = {
+  "entries": [
+    {
+      "id": "1111111",
+      "place": "New York",
+      "eatLocation": "A really cool diner",
+      "sleepLocation": "A friend's house",
+      "publishDate": 1470016976609
+    },
+    {
+      "id": "2222222",
+      "place": "Steamboat Springs",
+      "eatLocation": "Creekside Cafe",
+      "sleepLocation": "Camped halfway up a dirt road outside of town",
+      "publishDate": 1470016976609
+    },
+    {
+      "place": "Guadalajara",
+      "eatLocation": "On the beach",
+      "sleepLocation": "At a hostel",
+      "publishDate": 1470016976609
+    },
+  ]
+};
+
+function getRecentStatusUpdates(callbackFn) {
+  setTimeout(function () { callbackFn(mock_entries) }, 100);
+}
+
+// this function stays the same when we connect
+// to real API later
+function displayStatusUpdates(data) {
+  for (index in data.entries) {
+    $('body').append(
+      '<p>Place: ' + data.entries[index].place + '</p><p>Where we ate: ' + data.entries[index].eatLocation + '</p>');
+  }
+}
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayStatusUpdates() {
+  getRecentStatusUpdates(displayStatusUpdates);
+}
+
+$(function () {
+  getAndDisplayStatusUpdates();
+})
