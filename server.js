639beb08d38bb094e935e7693953d8ef82bf2ef8
Mark Yapp <markwyapp@gmail.com>
2018-12-07 01:42:13 -0700

    html

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+
+
+// log the http layer
+app.use(morgan('common'));
+
 app.use(express.static('public'));
 
-
-app.get("/", (req, res) => {
-	const message = 'Hello world';
-	res.send(message);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
