b3361613b1bc3e7119be12bce3c4ffa114a870f7
Mark Yapp <markwyapp@gmail.com>
2018-12-08 22:22:48 -0700

    mongo CRUD operations

--- entriesRouter.js
+++ entriesRouter.js
@@ -4,56 +4,97 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-//import models
-const { Entries } = require('./models.js');
+const { Entry } = require('./models');
 
-// Seed some data
-Entries.create(
-  'New York', ['Great Deli', 'In the van', 'Would definately go back here']);
-Entries.create(
-  'Los Angeles', ['American Diner', 'At a friends house', 'Hard to stealth camp, would not recommend']);
 
-//CRUD routes
+//GET request to /blog-posts, return all
 router.get('/', (req, res) => {
-  console.log('sending the entries list');
-  res.json(Entries.get());
-});
+  Entry.find()
+    .then(entry => {
+      res.json({
+        entry: entry.map(entry => entry)
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+})
 
-router.post('/', (req, res) => {
-  // ensure `place`in the request body
-  const requiredFields = ['title', 'content', 'author'];
+//GET request to /blog-posts, find by id
+router.get('/:id', (req, res) => {
+  Entry.findById(req.params.id)
+    .then(entry => {
+      res.json(entry)
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+})
+
+//POST request
+router.post("/", jsonParser, (req, res) => {
+  console.log("POST request received")
+  const requiredFields = ["place", "notes"];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
   }
-  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
-  res.status(201).json(post);
+
+  Entry.create({
+    place: req.body.place,
+    eat: req.body.eat,
+    sleep: req.body.sleep,
+    notes: req.body.notes
+  })
+    .then(entry => res.status(201).json(entry))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
 });
 
-router.post("/entries", jsonParser, (req, res) => {
-  console.log(req.body);
-  const entry = Entries.create(req.body.location, req.body.details)
-  res.status(201).json(entry);
+//PUT request
+router.put("/:id", jsonParser, (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message =
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ["place", "eat", "sleep", "notes"];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Entry
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-router.put('/:id', jsonParser, (req, res) => {
-  //put code to work with data
-  //   Model.create()
-
-  // .update
-
-  BlogPosts.update(req.body);
-  res.send(`this will update post: ${req.body.id}`)
-})
-
-router.delete('/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  res.json(`Deleted blog post id: ${req.params.id}`);
+router.delete("/:id", (req, res) => {
+  Entry.findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
+
 
 module.exports = router;
 
--- models.js
+++ models.js
@@ -1,68 +1,37 @@
-const uuid = require('uuid');
+"use strict";
 
-//replace volatile with database storage
-//define models here
+const mongoose = require("mongoose");
 
+//feedback on mongoose connection
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected');
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
 
-// this module provides volatile storage, using a `ShoppingList`
-// and `Recipes` model. We haven't learned about databases yet,
-// so for now we're using in-memory storage. This means each time
-// the app stops, our storage gets erased.
+const entrySchema = mongoose.Schema({
 
-// don't worry to much about how `ShoppingList` and `Recipes`
-// are implemented. Our concern in this example is with how
-// the API layer is implemented, and getting it to use an
-// existing model.
+  place: { type: String, required: true },
+  eat: String,
+  sleep: String,
+  notes: String
+})
 
+// entrySchema.methods.serialize = function () {
+//   return {
+//     author: this.author.firstName + ' ' + this.author.lastName,
+//     title: this.title,
+//     content: this.content
+//   }
+// }
 
-function StorageException(message) {
-  this.message = message;
-  this.name = "StorageException";
-}
+const Entry = mongoose.model("Entry", entrySchema, 'entries');
 
-function createEntries() {
-  const storage = Object.create(Entries);
-  storage.items = {};
-  return storage;
-}
-
-const Entries = {
-  create: function (location, details) {
-    console.log('Creating a new entry');
-    const item = {
-      location: location,
-      id: uuid.v4(),
-      details: details
-    };
-    this.items[item.id] = item;
-    return item;
-  },
-  get: function () {
-    return Object.keys(this.items).map(key => this.items[key]);
-  },
-  delete: function (itemId) {
-    console.log(`Deleting recipe with id \`${itemId}\``);
-    delete this.items[itemId];
-  },
-  update: function (updatedItem) {
-    console.log(`Updating recipe with id \`${updatedItem.id}\``);
-    const { id } = updatedItem;
-    if (!(id in this.items)) {
-      throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.items[updatedItem.id] = updatedItem;
-    return updatedItem;
-  }
-};
+module.exports = { Entry };
 
 
-function createEntries() {
-  const storage = Object.create(Entries);
-  storage.items = {};
-  return storage;
-}
 
-module.exports = {
-  Entries: createEntries()
-}
