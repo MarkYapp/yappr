c0bbf0d819296c8f81ed7b6bf23eb37ffc64c3a9
Mark Yapp <markwyapp@gmail.com>
2019-01-20 20:56:50 -0700

    fixed username input case

--- public/auth.js
+++ public/auth.js
@@ -1,94 +1,94 @@
 //page refresh
 $(function checkForAuth() {
-  let authToken = localStorage.getItem('authToken')
+  let authToken = localStorage.getItem("authToken");
   if (authToken) {
     showDashboard();
   }
 });
 
 function createNewUser(newUserInfo) {
-  fetch('/api/users',
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(newUserInfo)
-    })
+  fetch("/api/users", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify(newUserInfo)
+  })
     .then(response => {
-      $('.signup-status').text("Signup successful. Please log in.")
-      return response.json()
+      $(".signup-status").text("Signup successful. Please log in.");
+      return response.json();
     })
-    .then(function (response) {
+    .then(function(response) {
       if (!response.ok) {
-        $('.signup-status').text(response.message);
+        $(".signup-status").text(response.message);
       }
     })
-    .catch(error => console.log('Bad request'));
-};
+    .catch(error => console.log("Bad request"));
+}
 
 $(function listenForSignup() {
-  $('form[name=signup-user]').on('submit', function (event) {
+  $("form[name=signup-user]").on("submit", function(event) {
     event.preventDefault();
     let newUserInfo = {};
-    newUserInfo.username = $('#new-username').val();
-    newUserInfo.password = $('#new-password').val();
+    newUserInfo.username = $("#new-username").val();
+    newUserInfo.password = $("#new-password").val();
     createNewUser(newUserInfo);
-    $('#new-username').val('');
-    $('#new-password').val('');
+    $("#new-username").val("");
+    $("#new-password").val("");
   });
 });
 
 function logInUser(userInfo) {
-  fetch('/api/auth/login',
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(userInfo)
-    })
+  fetch("/api/auth/login", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify(userInfo)
+  })
     .then(response => {
-      return response.json()
+      return response.json();
     })
     .then(data => {
-      localStorage.setItem('authToken', data.authToken);
+      localStorage.setItem("authToken", data.authToken);
       showDashboard();
       getEntries();
     })
     .catch(error => {
-      $('.invalid-login-modal').removeClass('hidden');
-      $('#username').val('');
-      $('#password').val('');
-    })
-};
+      $(".invalid-login-modal").removeClass("hidden");
+      $("#username").val("");
+      $("#password").val("");
+    });
+}
 
 $(function hideInvalidUserModal() {
-  $('.invalid-modal-close-button').click(function (event) {
+  $(".invalid-modal-close-button").click(function(event) {
     event.preventDefault();
-    $('.invalid-login-modal').addClass('hidden');
+    $(".invalid-login-modal").addClass("hidden");
   });
 });
 
 $(function listenForLogin() {
-  $('form[name=login-user]').on('submit', function (event) {
+  $("form[name=login-user]").on("submit", function(event) {
     event.preventDefault();
     let userInfo = {};
-    userInfo.username = $('#username').val();
-    userInfo.password = $('#password').val();
+    userInfo.username = $("#username")
+      .val()
+      .toLowerCase();
+    userInfo.password = $("#password").val();
     logInUser(userInfo);
   });
 });
 
 $(function swapForms() {
-  $('main').on('click', '#signup-link', function (event) {
-    $('.login-form').addClass('hidden');
+  $("main").on("click", "#signup-link", function(event) {
+    $(".login-form").addClass("hidden");
     // $('.signup-form');
-    $('.signup-form').prop('hidden', false);
-  })
-  $('main').on('click', '#login-link', function (event) {
+    $(".signup-form").prop("hidden", false);
+  });
+  $("main").on("click", "#login-link", function(event) {
     $(".login-form").removeClass("hidden");
     $(".signup-form").addClass("hidden");
   });
@@ -97,9 +97,11 @@ $(function swapForms() {
 function showDashboard() {
   $(".login-form").addClass("hidden");
   $(".signup-form").addClass("hidden");
-  $('.icon-large').addClass('hidden').removeClass('icon-large');
-  $('.user-dashboard').prop('hidden', false);
-  $('main').addClass('dashboard');
-  $('body').addClass('dashboard');
+  $(".icon-large")
+    .addClass("hidden")
+    .removeClass("icon-large");
+  $(".user-dashboard").prop("hidden", false);
+  $("main").addClass("dashboard");
+  $("body").addClass("dashboard");
   getEntries();
-};
+}
