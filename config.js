b8390093facc342f18316718beb5eb4f5c6c0efb
Mark Yapp <markwyapp@gmail.com>
2020-11-23 22:00:45 -0500

    updated config vars

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 exports.DATABASE_URL =
-  process.env.DATABASE_URL ||
-  global.DATABASE_URL ||
+  process.env.MONGODB_URI ||
+  global.MONGODB_URI ||
   'mongodb://localhost/yappr-data';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-yappr-data";
